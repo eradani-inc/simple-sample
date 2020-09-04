ca079489a10ebdcc1458f2a4038e39569b42f056
amagid <aaron@eradani.com>
2020-09-03 19:30:47 -0700

    chore(Setup): Initial working version

---
+++ index.js
@@ -0,0 +1,12 @@
+const port = parseInt(process.argv[2]) || 3001;
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Up and Running!' }));
+
+const server = http.createServer(app);
+server.listen(port);
+
+console.log('Server listening on port ' + port);
